1c92f0b208f47c59f84219f81b24c1e25aa20141
anasraza57 <2f4BC6nwdP87iy>
2020-03-19 03:33:45 +0500

    db added

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from random import randint
+
 from flask import *
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin1@mysingleserver:Tempass12@mysingleserver.postgres.database.azure.com:5432/intellisupermart'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin1@mysingleserver:Tempass12@mysingleserver.postgres.database.azure.com:5432/intellisupermart'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -21,17 +24,67 @@ class Product(db.Model):
     product_name = db.Column(db.String(200))
     product_description = db.Column(db.String(1000))
     product_price = db.Column(db.String(200))
-    description = db.Column(db.String(1000))
     picture_id = db.Column(db.Integer)
     product_quantity = db.Column(db.String(20))
     prod_subcategory_id = db.Column(db.Integer)
+    slug = db.Column(db.String(200))
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    category_id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(200))
+    picture_id = db.Column(db.Integer)
+    slug = db.Column(db.String(200))
+
+
+class Subcategory(db.Model):
+    __tablename__ = 'subcategory'
+    subcategory_id = db.Column(db.Integer, primary_key=True)
+    subcategory_name = db.Column(db.String(200))
+    category_id = db.Column(db.Integer)
+    slug = db.Column(db.String(200))
+
+
+class Picture(db.Model):
+    __tablename__ = 'picture'
+    picture_id = db.Column(db.Integer, primary_key=True)
+    picture_url = db.Column(db.String(200))
 
 
 @app.route('/')
 def index():
     products = Product.query.all()
-    print(len(products))
-    return render_template('index.html')
+    no_of_prod = len(products)
+    categories = Category.query.all()
+    subcategories = Subcategory.query.all()
+    pictures = Picture.query.all()
+    best_seller_products_cat_index = [7, 6, 5, 4, 3, 2, 1, 0]
+
+    best_seller_products = []
+    if no_of_prod > 0:
+        for i in range(0, 8):
+            rand_index = randint(0, no_of_prod - 1)
+            rand_row = products[rand_index]
+            best_seller_products.append(rand_row)
+
+    best_seller_products_images = []
+    for product in best_seller_products:
+        for picture in pictures:
+            if product.picture_id == picture.picture_id:
+                best_seller_products_images.append(picture)
+
+    best_seller_products_categories = []
+    for item in best_seller_products:
+        for subcategory in subcategories:
+            if item.prod_subcategory_id == subcategory.subcategory_id:
+                for category in categories:
+                    if subcategory.category_id == category.category_id:
+                        best_seller_products_categories.append(category)
+
+    return render_template('index.html', categories=categories, pictures=pictures, products=products,
+                           best_seller_products_categories=best_seller_products_categories, index=best_seller_products_cat_index,
+                           best_seller_products=best_seller_products, best_seller_products_images=best_seller_products_images)
 
 
 @app.route('/about-us')
