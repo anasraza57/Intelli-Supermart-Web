3c762a3b47f8a47dbbc97b33fa7d4ba776eb1284
anasraza57 <2f4BC6nwdP87iy>
2020-05-21 04:21:31 +0500

    deploying test

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/anas_raza/FYP/Intelli-Supermart-Web/env'
+set -gx VIRTUAL_ENV '/home/anas_raza/Projects/Intelli-Supermart-Web/env'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
